124556272e35962b0d91c6d84e39ef002a5a23ff
Muna Abdelwahab <69257240+MunaAbdelwahab@users.noreply.github.com>
2024-09-30 19:30:25 +0300

    Initial Commit

---
+++ Business Card Challenge/Business_Card_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_Card_ChallengeApp.swift
+//  Business Card Challenge
+//
+//  Created by MSOL on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Business_Card_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
